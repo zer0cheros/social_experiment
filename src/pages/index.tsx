15780d07d432dde0b1f378d4285a6a30b2c79dd6
Viglot <vigviglot36.hiltunen@gmail.com>
2023-01-18 08:56:04 +0200

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='bg-red-500'>
+      <h1 className='text-bold text-xl font-extralight'>Social experiment with tailwind and framer motion</h1>
+    </div>
+  )
+}
