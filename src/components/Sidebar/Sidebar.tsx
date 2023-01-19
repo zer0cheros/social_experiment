237fe324649e20f4f50b6c2394c8311ebed65f03
TurkPatrik <struganhd@gmail.com>
2023-01-19 09:56:45 +0200

    jå

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    
+    <div className="bg-gray-900 h-screen w-64 flex-none">
+  <nav>
+    <a href="#" className="block px-4 py-2 text-lg font-medium leading-5 text-white hover:text-gray-300">Home</a>
+    <a href="#" className="block px-4 py-2 text-base font-medium leading-5 text-white hover:text-gray-300">About</a>
+    <a href="#" className="block px-4 py-2 text-base font-medium leading-5 text-white hover:text-gray-300">Contact</a>
+  </nav>
+</div>
+
+
   )
 }
 
