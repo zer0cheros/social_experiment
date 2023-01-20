66cc21f69719fd5bb53c567a1252c2c89944b5c3
Viglot <vigviglot36.hiltunen@gmail.com>
2023-01-20 13:25:54 +0200

    third commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import {motion} from 'framer-motion'
+
 export default function Home() {
   return (
-    <div className='bg-red-500'>
+    <motion.div initial={{opacity: 0}}  animate={{opacity: 1, y: 20}} transition={{ duration: 2}} className='bg-red-500'>
       <h1 className='text-bold text-xl font-extralight'>Social experiment with tailwind and framer motion</h1>
       
-    </div>
+    </motion.div>
   )
 }
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,32 +1,32 @@
 import React from 'react'
+import {motion} from 'framer-motion'
 
 const index = () => {
    
   return (
     <div className='flex justify-center bg-Gunmetal h-100 w-100 font-mono pt-10'>
-        <div className='text-center border-2 bg-Liberty w-40 h-50 rounded'>
+        <motion.div initial={{opacity: 0}}  animate={{opacity: 1, y: 30}} transition={{ duration: 2}} className='text-center border-2 bg-Liberty w-40 h-50 rounded'>
             <p className='text-4xl mt-6'>Register</p>
 
-            <div>
+            <motion.div>
             <br />
-            <input placeholder='Gmail' type='text' className=' mt-10 w-23 h-10 rounded'/>
+            <input placeholder='Email' id='email' type='email' className=' mt-10 w-23 h-10 rounded'/>
             <br />
-            <input placeholder='Name' type="text" className='mt-8 w-23 h-10 rounded'/>
+            <input placeholder='Name' id='name' type="text" className='mt-8 w-23 h-10 rounded'/>
             <br />
-            <input placeholder='Username' type='text' className='mt-8 w-23 h-10 rounded'/>
+            <input placeholder='Username' id='username' type='text' className='mt-8 w-23 h-10 rounded'/>
             <br />
-            <input placeholder='Password' type="password" className='mt-8 w-23 h-10 rounded' id='password'/>
+            <input placeholder='Password' id='password' type="password" className='mt-8 w-23 h-10 rounded'/>
             <br />
-            <input placeholder='Confirm Password' type='password' className='mt-8 w-23 h-10 rounded'/>
+            <input placeholder='Confirm Password' id='conpassword' type='password' className='mt-8 w-23 h-10 rounded'/>
             <br />
-            </div>
+            </motion.div>
 
             <div className='mt-10'>
                 <button className='bg-Glaucous border text-white w-23 h-10 rounded'>Register</button>
             </div>
             <br />
-            <p className='text-white'>Have you forgot your password?</p>
-        </div>
+        </motion.div>
     </div>
   )
 }
