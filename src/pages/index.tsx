0648ecd515ae0f4861759eed7d37acfb35f34454
FilipK03 <71625347+FilipK03@users.noreply.github.com>
2023-01-25 10:11:05 +0200

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Follows from "./follows/Follows";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className='text-bold text-xl'>Social experiment with tailwind and framer motion</h1>
+      <div className="w-full h-screen flex justify-center items-center bg-EerieBlack">
+      <div className='w-1/3 h-screen flex-col flex justify-center items-center '>
+      <Follows />
+      <Follows />
+      <Follows />
+      <Follows />
+      <Follows />
+      <Follows />
+      </div>
+      </div>
+    </>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Glaucous': '#7289DA',
+        'Liberty': '#465CAB',
+        'Onyx': '#424549',
+        'Gunmetal': '#282B30',
+        'EerieBlack': '#1E2124',
+        'Ruby': '#D81E5B',
+      }
+    },
   },
   plugins: [],
 }
