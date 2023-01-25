ac0de787bfa342394f624624062395a7d1cbe27e
Joskitt <71625292+Joskitt@users.noreply.github.com>
2023-01-25 10:23:39 +0200

    dfghdfg

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: "Roboto, sans-serif",
+        heading: '"Roboto Slab", sans-serif'
+      }
+    },
+    colors: {
+      "black": "#1E2124",
+      "darkgrey": "#282B30",
+      "grey": "#424549",
+     "darkblue": "#465CAB",
+     "blue": "#7289DA",
+     "red": "#D81E5B",
+     "white": "#C9C9C9"
+    }
   },
   plugins: [],
 }
