b9099ae32b4002b5443aca6718bdfaac2a0987eb
FilipK03 <71625347+FilipK03@users.noreply.github.com>
2023-01-25 13:23:34 +0200

    styling

--- src/pages/follows/index.tsx
+++ src/pages/follows/index.tsx
@@ -4,8 +4,11 @@ export default function Home() {
   return (
     <>
       <h1 className='text-bold text-xl'>Social experiment with tailwind and framer motion</h1>
-      <div className="w-full h-screen flex justify-center items-center bg-EerieBlack">
+      <div className="w-full  h-screen flex justify-center items-center bg-EerieBlack">
       <div className='w-1/3 h-screen flex-col flex justify-center items-center '>
+      <FollowBox />
+      <FollowBox />
+      <FollowBox />
       <FollowBox />
 
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      minWidth: {
+        '0': '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        'full': '100%',
+        '100': '100px',
+        '200': '200px',
+        '300': '300px',
+        '400': '400px',
+        '500': '500px',
+        '600': '600px',
+        },
       colors: {
         'Glaucous': '#7289DA',
         'Liberty': '#465CAB',
@@ -13,6 +26,7 @@ module.exports = {
         'Gunmetal': '#282B30',
         'EerieBlack': '#1E2124',
         'Ruby': '#D81E5B',
+        'RubyLight': '#db346b',
       }
     },
   },
