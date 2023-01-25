3c85488e91e16f702c2cb6003371ae73a100e14a
Zer0cheros <christian.wiss@optimaedu.fi>
2023-01-25 14:52:28 +0200

    aa

--- src/pages/follows/index.tsx
+++ src/pages/follows/index.tsx
@@ -2,7 +2,6 @@ import FollowBox from "../../components/FollowBox";
 import prisma from "../../core/prisma";
 
 export default function Home() {
-
   return (
       <div className="w-full min-w-600 h-screen flex justify-center items-center bg-EerieBlack">
       <div className='w-1/3 h-screen flex-col flex justify-center items-center '>
@@ -10,7 +9,6 @@ export default function Home() {
       <FollowBox />
       <FollowBox />
       <FollowBox />
-
       </div>
       </div>
   )
