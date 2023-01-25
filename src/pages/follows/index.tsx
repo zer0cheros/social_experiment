e8ba7c5481f1dcbc3389d1c6a6f39ce777ce07b6
FilipK03 <71625347+FilipK03@users.noreply.github.com>
2023-01-25 14:49:26 +0200

    styling

--- src/pages/follows/index.tsx
+++ src/pages/follows/index.tsx
@@ -1,10 +1,11 @@
 import FollowBox from "../../components/FollowBox";
+import Topbar_comp from "../../components/Topbar/Topbar_comp";
 
 export default function Home() {
   return (
     <>
-      <h1 className='text-bold text-xl'>Social experiment with tailwind and framer motion</h1>
-      <div className="w-full min-w-600 h-screen flex justify-center items-center bg-EerieBlack">
+      <Topbar_comp />
+      <div className="w-full min-w-600 h-screen flex justify-center items-center bg-Gunmetal">
       <div className='w-1/3 h-screen flex-col flex justify-center items-center '>
       <FollowBox />
       <FollowBox />
