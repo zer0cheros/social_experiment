be1982de625ad82ed98b3894ea148a175b86fe1d
Viglot <vigviglot36.hiltunen@gmail.com>
2023-01-27 10:55:20 +0200

    little things

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -4,15 +4,13 @@ import {motion} from 'framer-motion'
 const index = () => {
    
   return (
-    <div className='flex justify-center bg-Gunmetal h-100 w-100 font-mono pt-10'>
+    <div className='flex justify-center bg-Gunmetal h-100 w-100 font-mono pt-15'>
         <motion.div initial={{opacity: 0}}  animate={{opacity: 1, y: 30}} transition={{ duration: 2}} className='text-center border-2 bg-Liberty w-40 h-50 rounded'>
-            <p className='text-4xl mt-6'>Register</p>
+            <p className='text-4xl mt-8'>Register</p>
 
             <motion.div>
             <br />
             <input placeholder='Email' id='email' type='email' className=' mt-10 w-23 h-10 rounded'/>
-            <br />
-            <input placeholder='Name' id='name' type="text" className='mt-8 w-23 h-10 rounded'/>
             <br />
             <input placeholder='Username' id='username' type='text' className='mt-8 w-23 h-10 rounded'/>
             <br />
@@ -25,6 +23,10 @@ const index = () => {
             <div className='mt-10'>
                 <button className='bg-Glaucous border text-white w-23 h-10 rounded'>Register</button>
             </div>
+            <br />
+            <p className='pt-6'>Already have an account?</p>
+            <br />
+            <a href="">Click Here</a>
             <br />
         </motion.div>
     </div>
