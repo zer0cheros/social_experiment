4436cf70881409d03c817a032a941c2f9348a50e
Zer0cheros <christian.wiss@optimaedu.fi>
2023-01-31 18:00:03 +0200

    a

--- src/components/handleUsers/GetUsers.tsx
+++ src/components/handleUsers/GetUsers.tsx
@@ -15,7 +15,7 @@ const GetUsers:React.FC<{users: User[]}> = ({users}) => {
     }
   return (
     <div className='bg-Liberty h-screen absolute top-0 right-0 w-48 text-center m-auto'>
-        <h1 className='text-semibold text-xl text-white '>Find a Friend</h1>
+        <h1 className='text-semibold text-xl text-white relative top-[6vh] mb-6 '>Find a Friend</h1>
         {users.map(user=>(
             <div key={user.id} className='shadow-xl rounded-md p-11 relative m-4'>
             <h1 className='text-semibold text-lg text-white '>{user.name}</h1>
